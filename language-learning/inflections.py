2b67b80af8a397dff8866efb0193dedc0d8bb603
Carl Davidson <davidson16807@gmail.com>
2024-09-01 14:19:05 -0600

    further expansion to definitions
    GLOSSARY.tsv should now represent all concepts mentioned in code

--- language-learning/inflections.py
+++ language-learning/inflections.py
@@ -19,7 +19,6 @@ from tools.shorthands import (
 
 from tools.parsing import SeparatedValuesFileParsing, TermParsing, ListParsing, LatexlikeParsing
 from tools.annotation import RowAnnotation, CellAnnotation
-from tools.predicates import Predicate
 from tools.dictstores import DefaultDictLookup, DictLookup, DictSet, NestedDictLookup, FallbackDictLookup, ProceduralLookup
 from tools.indexing import DictTupleIndexing, DictKeyIndexing
 from tools.labels import TermLabelEditing, TermLabelFiltering
